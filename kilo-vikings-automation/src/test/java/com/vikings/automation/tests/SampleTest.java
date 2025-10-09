3bd4afd74d00bb2890189d8045bcd03b8935f61f
Aniket Sagane <sagane.aniket19@gmail.com>
2025-10-09 15:58:59 +0530

    Initial automation testing framework setup

---
+++ kilo-vikings-automation/src/test/java/com/vikings/automation/tests/SampleTest.java
@@ -0,0 +1,12 @@
+package com.vikings.automation.tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1, "Basic addition should work.");
+    }
+}
