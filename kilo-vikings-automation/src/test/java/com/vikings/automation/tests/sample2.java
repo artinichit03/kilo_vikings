93af25e180b25abf53d07a2db9bc899c06ea9183
Shriya1523 <deshmukhshriya014@gmail.com>
2025-10-10 00:51:21 +0530

    commit 2

---
+++ kilo-vikings-automation/src/test/java/com/vikings/automation/tests/sample2.java
@@ -0,0 +1,12 @@
+package com.vikings.automation.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class sample2 {
+	public static void main(String[] args) {
+		int a=10, b=15;
+		int c=a+b;
+		System.out.println("Value of c: "+c);
+	}
+
+}
