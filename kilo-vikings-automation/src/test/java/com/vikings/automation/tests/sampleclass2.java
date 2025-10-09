a2ca0e658ab8a545264da41a89b5e84833082d59
shrilesh-QA <rongheshrilesh@gmail.com>
2025-10-10 02:13:10 +0530

    commit 2

---
+++ kilo-vikings-automation/src/test/java/com/vikings/automation/tests/sampleclass2.java
@@ -0,0 +1,17 @@
+package com.vikings.automation.tests;
+
+public class sampleclass2 {
+
+	public static void main(String[] args) 
+	{
+	
+		int i,a,c;
+		i=10;
+		a=20;
+		c = i+a;
+		System.out.println(c);
+																		
+
+	}
+
+}
