0d9f83e394e9896d2f372bcb676616ea2a8c698a
shrilesh-QA <rongheshrilesh@gmail.com>
2025-10-10 02:33:48 +0530

    commit 3

---
+++ kilo-vikings-automation/src/test/java/com/vikings/automation/tests/SampleClass3.java
@@ -0,0 +1,16 @@
+package com.vikings.automation.tests;
+
+public class SampleClass3 {
+	
+	public static void main(String[]args)
+	{
+		int a,b,c,sum,add;
+		a=10;
+		b=20;
+		c=30;
+		sum=a+b+c;
+		add = sum/3;
+		System.out.println(add);
+	}
+
+}
