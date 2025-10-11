e2495bb65fe247477beec91810f5d3c564b8caf2
harshalikirde91-crypto <harshalikirde91@gmail.com>
2025-10-11 18:08:13 +0530

    This is my first Commit

---
+++ kilo-vikings-automation/src/test/java/com/vikings/automation/tests/SampleTest2.java
@@ -0,0 +1,13 @@
+package com.vikings.automation.tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest2 {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1, "Basic addition should work.");
+        System.out.println("Hello this is done by harshali");
+    }
+}
